10e08e490b520e7cc1d4f1931f41c267b89fa8b4
Austin M. Weber <133900499+weber1158@users.noreply.github.com>
2025-08-05 11:45:34 -0400

    Simplified examples using new functionality

--- MATLAB/Examples/xray_plot_examples.m
+++ MATLAB/Examples/xray_plot_examples.m
@@ -1,41 +1,27 @@
 % Copyright 2025 Austin M. Weber
-%% Clear
+
 close all
 clear,clc
 
-%% Define paths
-% Get current folder
-currentFolder = pwd;
-% Define path to data
-dataPath = [currentFolder '/example_spectra/'];
-% Define path to functions
-exStr = 'Examples';
-functionPath = currentFolder(1:end-length(exStr));
-addpath(functionPath)
-
-%% Import data
-% Define file extension
-ext = '.msa';
-
-% Import spectral data
-ab = read_msa([dataPath 'albite' ext]);
-ap = read_msa([dataPath 'apatite' ext]);
-aug= read_msa([dataPath 'augite' ext]);
-bt = read_msa([dataPath 'biotite' ext]);
-chl= read_msa([dataPath 'chlorite' ext]);
-en = read_msa([dataPath 'enstatite' ext]);
-hbl= read_msa([dataPath 'hornblende' ext]);
-kln= read_msa([dataPath 'kaolinite' ext]);
-lab= read_msa([dataPath 'labradorite' ext]);
-mc = read_msa([dataPath 'microcline' ext]);
-mnt= read_msa([dataPath 'montmorillonite' ext]);
-ms = read_msa([dataPath 'muscovite' ext]);
-olig=read_msa([dataPath 'oligoclase' ext]);
-plg =read_msa([dataPath 'palygorskite' ext]);
-pgt =read_msa([dataPath 'pigeonite' ext]);
-spn =read_msa([dataPath 'sphene' ext]);
-spl =read_msa([dataPath 'spinel' ext]);
-vrm =read_msa([dataPath 'vermiculite' ext]);
+%% Import spectral data
+ab = read_msa('albite.msa');
+ap = read_msa('apatite.msa');
+aug= read_msa('augite.msa');
+bt = read_msa('biotite.msa');
+chl= read_msa('chlorite.msa');
+en = read_msa('enstatite.msa');
+hbl= read_msa('hornblende.msa');
+kln= read_msa('kaolinite.msa');
+lab= read_msa('labradorite.msa');
+mc = read_msa('microcline.msa');
+mnt= read_msa('montmorillonite.msa');
+ms = read_msa('muscovite.msa');
+olig=read_msa('oligoclase.msa');
+plg =read_msa('palygorskite.msa');
+pgt =read_msa('pigeonite.msa');
+spn =read_msa('sphene.msa');
+spl =read_msa('spinel.msa');
+vrm =read_msa('vermiculite.msa');
 
 %% Visualize biotite spectrum with labels
 figure(1)
@@ -46,59 +32,34 @@ title('Biotite  K_2(Mg,Fe^{2+})_{6-4}(Fe^{3+},Al,Ti)_{0-2}[Si_{6-5}Al_{2-3}O_{20
 % and show all possible elements for each peak
 xray_peak_label(plt,88,[],'all')
 
-%% Visualize all spectra using normalized units
+%% Visualize multiple spectra in a single plot
 figure(2)
-% Need to be in reverse order if you want the legend to display
-% alphabetically
-xray_plot(norm_spectra(vrm));
-hold on
- xray_plot(norm_spectra(spl));
- xray_plot(norm_spectra(spn));
- xray_plot(norm_spectra(pgt));
- xray_plot(norm_spectra(plg));
- xray_plot(norm_spectra(olig));
- xray_plot(norm_spectra(ms));
- xray_plot(norm_spectra(mnt));
- xray_plot(norm_spectra(mc));
- xray_plot(norm_spectra(lab));
- xray_plot(norm_spectra(kln));
- xray_plot(norm_spectra(hbl));
- xray_plot(norm_spectra(en));
- xray_plot(norm_spectra(chl));
- xray_plot(norm_spectra(bt));
- xray_plot(norm_spectra(aug));
- xray_plot(norm_spectra(ap));
- xray_plot(norm_spectra(ab));
-hold off
+p1 = xray_plot(ab);
+p2 = add_xray_plot(p1,ap);
+p3 = add_xray_plot(p2,aug);
+p4 = add_xray_plot(p3,bt);
+p5 = add_xray_plot(p4,chl);
+p6 = add_xray_plot(p5,en);
+p7 = add_xray_plot(p6,hbl);
+p8 = add_xray_plot(p7,kln);
+p9 = add_xray_plot(p8,lab);
+p10= add_xray_plot(p9,mc);
+p11= add_xray_plot(p10,mnt);
+p12= add_xray_plot(p11,ms);
+p13= add_xray_plot(p12,olig);
+p14= add_xray_plot(p13,plg);
+p15= add_xray_plot(p14,pgt);
+p16= add_xray_plot(p15,spl);
+p17= add_xray_plot(p16,spn);
+p18= add_xray_plot(p17,vrm);
 
 % Add a legend
-labels = flip({'Ab','Ap','Aug',...
+labels = {'Ab','Ap','Aug',...
     'Bt','Chl','En',...
     'Hbl','Kln','Lab',...
     'Mc','Mnt','Ms',...
     'Olig','Plg','Pgt',...
-    'Spl','Spn','Vrm'});
+    'Spl','Spn','Vrm'};
 L=legend(gca,labels);
 L.NumColumns=3;
 L.Title.String='Mineral';
-
-%% Local function
-function ns = norm_spectra(s)
-%Normalize spectral data from 0 to 1
-%
-%SYNTAX
-% ns = norm_spectra(s)
-%
-%INPUT
-% s::{Table} X-ray data imported using read_msa()
-%
-%OUTPUT
-% ns::{Table} Normalized table of the data in s
-%
-%BEGIN FUNCTION BODY
- counts = s.Counts;
- counts_norm = normalize(counts,'Range',[0 1]);
- s.Counts = counts_norm;
- ns = s;
-% END FUNCTION BODY
-end
